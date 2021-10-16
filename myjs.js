09c6760641e473d1d7b218ad83b964a551863f3d
samira talanchi <samira73talanchi@gmail.com>
2021-10-16 17:30:30 +0330

    add styles

--- myjs.js
+++ myjs.js
@@ -6,9 +6,11 @@ window.onload=function() {
     let minutes= parseInt(dif/60000);
     let hour,day,month,year ;
     let fContent = "created "
+    // get minutes
     if (minutes < 60) {
         fContent += minutes + " minutes";
     } else {
+        // get hours
         hour = parseInt(minutes/60);
         if (hour < 24) {
             if (hour == 1) {
@@ -17,6 +19,7 @@ window.onload=function() {
                 fContent += hour + " hours";
             }
         } else {
+            // get days
             day = parseInt(hour/24);
             if (day < 30) {
                 if (day == 1) {
@@ -25,6 +28,7 @@ window.onload=function() {
                     fContent += day + " days";
                 }
             } else {
+                // get monthes
                 month = parseInt(day/30);
                 if (month < 12){
                     if (month == 1) {
@@ -33,31 +37,46 @@ window.onload=function() {
                         fContent += month + " month";
                     }
                 } else {
+                    // get years
                     year = parseInt(month/12);
                     if (year == 1) {
                         fContent += year + " year";
                     } else {
                         fContent += year + " years";
                     }
-                }
-                
+                } 
             }
-            
         } 
-
     }
     fContent += " ago";
     footer.innerHTML= fContent;
 }
 function darkmode() {
     let switcher = document.getElementById("myswitcher");
-    let cnt = document.getElementById("content");
-    let vw = document.getElementById("view");
+
     if (switcher.checked==true) {
-        vw.innerHTML="Dark mode is enable";
+        document.getElementsByTagName("body")[0].style.backgroundColor="#333333";
+
+        document.getElementById("content").style.color="white";
+
+        document.getElementById("card").style.borderColor="white";
+
+        document.getElementsByClassName("card-header")[0].style.backgroundColor="#222222";
+        document.getElementsByClassName("card-footer")[0].style.backgroundColor="#222222";
+        document.getElementsByClassName("card-body")[0].style.backgroundColor="#444444";
+        document.getElementsByClassName("card-body")[0].style.color="white";
     }
     else {
-        vw.innerHTML="Light mode is enable";
+        document.getElementsByTagName("body")[0].style.backgroundColor="#ffffff";
+
+        document.getElementById("content").style.color="black";
+
+        document.getElementById("card").style.borderColor="#1260cc";
+
+        document.getElementsByClassName("card-header")[0].style.backgroundColor="#1260cc";
+        document.getElementsByClassName("card-footer")[0].style.backgroundColor="#1260cc";
+        document.getElementsByClassName("card-body")[0].style.backgroundColor="#ffffff";
+        document.getElementsByClassName("card-body")[0].style.color="black";
     }
 } 
 
