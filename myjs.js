8306d39d836511cd9a903dabda5ed600d1192e00
samira talanchi <samira73talanchi@gmail.com>
2021-10-16 16:49:50 +0330

    creat timer of creation time

--- myjs.js
+++ myjs.js
@@ -1,3 +1,54 @@
+window.onload=function() {
+    let footer = document.getElementById("footer-content");
+    let nDate = new Date();
+    let wDate = new Date(2021,09,16,16,11,0);
+    let dif = Date.parse(nDate)-Date.parse(wDate);
+    let minutes= parseInt(dif/60000);
+    let hour,day,month,year ;
+    let fContent = "created "
+    if (minutes < 60) {
+        fContent += minutes + " minutes";
+    } else {
+        hour = parseInt(minutes/60);
+        if (hour < 24) {
+            if (hour == 1) {
+                fContent += hour + " hour";
+            } else {
+                fContent += hour + " hours";
+            }
+        } else {
+            day = parseInt(hour/24);
+            if (day < 30) {
+                if (day == 1) {
+                    fContent += day + " day";
+                } else {
+                    fContent += day + " days";
+                }
+            } else {
+                month = parseInt(day/30);
+                if (month < 12){
+                    if (month == 1) {
+                        fContent += month + " month";
+                    } else {
+                        fContent += month + " month";
+                    }
+                } else {
+                    year = parseInt(month/12);
+                    if (year == 1) {
+                        fContent += year + " year";
+                    } else {
+                        fContent += year + " years";
+                    }
+                }
+                
+            }
+            
+        } 
+
+    }
+    fContent += " ago";
+    footer.innerHTML= fContent;
+}
 function darkmode() {
     let switcher = document.getElementById("myswitcher");
     let cnt = document.getElementById("content");
